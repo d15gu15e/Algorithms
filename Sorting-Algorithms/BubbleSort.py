471b0d5d45e37598c73d7bfa413a819a13cb0f44
d15gu15e <92394428+d15gu15e@users.noreply.github.com>
2021-10-31 13:09:31 +0100

    Update BubbleSort.py

--- Sorting-Algorithms/BubbleSort.py
+++ Sorting-Algorithms/BubbleSort.py
@@ -1,1 +1,23 @@
+'''
 
+— How does Bubble Sort work?
+
+Bubble Sort is one of the easiest to understand algorithms:
+Start from the leftmost element, compare it to the neighboring
+element to the right, and put them in the correct order. 
+
+— When should I use this sorting algorithm?
+
+Honestly? Never. With an average complexity of О(n²), it's incredibly
+slow, especially when handling a lot of data.
+
+'''
+
+def bubbleSort(arr):
+    # Go through every item in the list
+    for i in range(len(arr)):
+        # We want the last pair of adjacent elements to be (n-2, n-1)
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j+1]:
+                # Swap the elements if they are not in the right order.
+                arr[j], arr[j+1] = arr[j+1], arr[j]
