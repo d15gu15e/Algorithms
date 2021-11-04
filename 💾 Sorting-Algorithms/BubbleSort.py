b17ba5013a79f2319094f0181736879684940f26
d15gu15e <92394428+d15gu15e@users.noreply.github.com>
2021-11-04 17:31:48 +0100

    Update BubbleSort.py
    — What is Bubble Sort?
    
    Bubblesort, sometimes called sinking sort, is a simple
    sorting algorithm. It is a simple algorithm, but inefficient. 
    It is often used as an illustration of an algorithm because
    of its simplicity and because it is easy to explain. 
    
    — How does Bubble Sort work?
    
    Start from the leftmost element, compare it to the neighboring
    element to the right, and put them in the correct order. 
    — When should I use this sorting algorithm?
    
    Honestly? Never. With an average complexity of О(n²), it's incredibly
    slow, especially when handling a lot of data.

--- 💾 Sorting-Algorithms/BubbleSort.py
+++ 💾 Sorting-Algorithms/BubbleSort.py
@@ -1,24 +1,3 @@
-'''
-
-— What is Bubble Sort?
-
-Bubblesort, sometimes called sinking sort, is a simple
-sorting algorithm. It is a simple algorithm, but inefficient. 
-It is often used as an illustration of an algorithm because 
-of its simplicity and because it is easy to explain. 
-
-— How does Bubble Sort work?
-
-Start from the leftmost element, compare it to the neighboring
-element to the right, and put them in the correct order. 
-
-— When should I use this sorting algorithm?
-
-Honestly? Never. With an average complexity of О(n²), it's incredibly
-slow, especially when handling a lot of data.
-
-'''
-
 def bubbleSort(arr):
     # Go through every item in the list
     for i in range(len(arr)):
